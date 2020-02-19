8c85e69b4a8706a09347e92729da529da65227a3
Julian Wakefield <julianwakefield@outlook.com>
2020-02-19 14:32:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
 counter = 0
 new_array = [ ]
 while array[counter] do
-puts array[counter]**2  <<new_array[0..2].to_i
+puts array[counter]**2  <<new_array[0..2]
 
 
 counter += 1
