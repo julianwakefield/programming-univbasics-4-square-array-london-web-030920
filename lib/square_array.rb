3a3457790b68b798f39eb6e0218ee0df7a94cc8b
Julian Wakefield <julianwakefield@outlook.com>
2020-02-19 14:13:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
-array = [1,2,3]
+
 end
 
 counter = 0
-array = [1,2,3]
+
 while array[counter] do
-puts new_numbers([counter]*2)
+puts array([counter]*2)
 counter += 1
 end
